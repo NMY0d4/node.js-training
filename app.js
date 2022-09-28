34aa096e188cf963c561cbb958aa40361c56a257
Grégory Marini <greg.marini@hotmail.fr>
2022-09-28 18:29:25 +0200

    initialisation package.json, instalation express paramétrage du server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello, Express! 👋"));
+
+app.listen(port, () =>
+    console.log(
+        `Notre application Node est démarré sur : http://localhost:${port}`
+    )
+);
