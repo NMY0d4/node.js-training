fdf00ed67dc04a27ec3ecd0cfa8e9dd10877f013
Grégory Marini <greg.marini@hotmail.fr>
2022-09-28 22:03:05 +0200

    ajout fichier 'mock-pokemon' et insertion dans app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+let pokemons = require("./mock-pokemon");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello, Express! 👋"));
+app.get("/", (req, res) => res.send("Hello, Express 2! 👋"));
+
+app.get("/api/pokemons/:id", (req, res) => {
+    const id = +req.params.id;
+    const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+    res.send(`Vous  avez demandé le pokemon ${pokemon.name}.`);
+});
 
 app.listen(port, () =>
     console.log(
